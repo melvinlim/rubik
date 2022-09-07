8193d503c48af74493b768f5631aeac0cdc13dea
Melvin Lim <dxlim@outlook.com>
2022-09-07 15:00:45 -0700

    changed underlying data type to char instead of int. changed display to cross shape.

--- rubik/rubik.cpp
+++ rubik/rubik.cpp
@@ -7,7 +7,10 @@
 #define CUBELENGTH 2
 #define CUBEWIDTH 2
 #define CUBESZ (CUBELENGTH * CUBEWIDTH * FACES)
-int cubeData[CUBESZ];
+
+#define ATTACHMENTPOINT 1	//where the sides of the cube are attached on the 2d display
+
+char cubeData[CUBESZ];
 
 HANDLE hCursor;
 
@@ -22,10 +25,10 @@ void writeCharAt(const char* text, COORD coord) {
 
 struct CubeFace {
 	int idx;
-	int* tl;
-	int* tr;
-	int* bl;
-	int* br;
+	char* tl;
+	char* tr;
+	char* bl;
+	char* br;
 	COORD tlpos;
 	COORD trpos;
 	COORD blpos;
@@ -39,7 +42,7 @@ struct Cube {
 Cube cube;
 
 void initCube() {
-	cubeData[0] = 1;
+	cubeData[0] = 'x';
 	for (int i = 0; i < FACES; i++) {
 		cube.cubeFace[i].idx = i;
 		cube.cubeFace[i].tl = &cubeData[0];
@@ -64,47 +67,40 @@ void initCube() {
 			cube.cubeFace[i].trpos.X = 1;
 			cube.cubeFace[i].blpos.X = 0;
 			cube.cubeFace[i].brpos.X = 1;
-			cube.cubeFace[i].tlpos.Y = (3 * 2);
-			cube.cubeFace[i].trpos.Y = (3 * 2);
-			cube.cubeFace[i].blpos.Y = (3 * 2) + 1;
-			cube.cubeFace[i].brpos.Y = (3 * 2) + 1;
+			cube.cubeFace[i].tlpos.Y = (ATTACHMENTPOINT * 2);
+			cube.cubeFace[i].trpos.Y = (ATTACHMENTPOINT * 2);
+			cube.cubeFace[i].blpos.Y = (ATTACHMENTPOINT * 2) + 1;
+			cube.cubeFace[i].brpos.Y = (ATTACHMENTPOINT * 2) + 1;
 		}
 		if (i == 5) {
 			cube.cubeFace[i].tlpos.X = 4;
 			cube.cubeFace[i].trpos.X = 5;
 			cube.cubeFace[i].blpos.X = 4;
 			cube.cubeFace[i].brpos.X = 5;
-			cube.cubeFace[i].tlpos.Y = (3 * 2);
-			cube.cubeFace[i].trpos.Y = (3 * 2);
-			cube.cubeFace[i].blpos.Y = (3 * 2) + 1;
-			cube.cubeFace[i].brpos.Y = (3 * 2) + 1;
+			cube.cubeFace[i].tlpos.Y = (ATTACHMENTPOINT * 2);
+			cube.cubeFace[i].trpos.Y = (ATTACHMENTPOINT * 2);
+			cube.cubeFace[i].blpos.Y = (ATTACHMENTPOINT * 2) + 1;
+			cube.cubeFace[i].brpos.Y = (ATTACHMENTPOINT * 2) + 1;
 		}
 	}
 }
 
+const char* getCellValue(int x) {
+	char result[2];
+	sprintf_s(result, "%d", x);
+	return result;
+}
+
+void displayFace(int face) {
+	writeCharAt(cube.cubeFace[face].tl, cube.cubeFace[face].tlpos);
+	writeCharAt(cube.cubeFace[face].tr, cube.cubeFace[face].trpos);
+	writeCharAt(cube.cubeFace[face].bl, cube.cubeFace[face].blpos);
+	writeCharAt(cube.cubeFace[face].br, cube.cubeFace[face].brpos);
+}
+
 void displayCube() {
 	for (int face = 0; face < FACES; face++) {
-		writeCharAt("x", cube.cubeFace[face].tlpos);
-		writeCharAt("x", cube.cubeFace[face].trpos);
-		writeCharAt("x", cube.cubeFace[face].blpos);
-		writeCharAt("x", cube.cubeFace[face].brpos);
-		/*
-		if (face < 3)
-			printf("  ");
-		if (face == 4)
-			printf("  ");
-		printf("%d", *cube.cubeFace[face].tl);
-		printf("%d", *cube.cubeFace[face].tr);
-		printf("\n");
-		if (face != 3)
-			printf("  ");
-		if (face == 4)
-			printf("  ");
-		printf("%d", *cube.cubeFace[face].bl);
-		printf("%d", *cube.cubeFace[face].br);
-		if (face != 3 && face != 4)
-			printf("\n");
-			*/
+		displayFace(face);
 	}
 }
 
